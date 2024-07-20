738edccca48c7224921b4cbf440ee8eb990756c7
saalim <ksaalim418@gmail.com>
2024-07-20 16:37:41 +0530

    security group  block

---
+++ Vpc_Networking/securitygroup.tf
@@ -0,0 +1,38 @@
+
+resource "aws_security_group" "sg" {
+  name        = "tcw_security_group"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.tcw-vpc.id
+
+  ingress = [
+    {
+      description      = "All traffic"
+      from_port        = 0    # All ports
+      to_port          = 0    # All Ports
+      protocol         = "-1" # All traffic
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = null
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      description      = "Outbound rule"
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+
+  tags = {
+    Name = "tcw_security_group"
+  }
+}
