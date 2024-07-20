2f49dd827f4ca2f40dbdb71ed2033b5b27eb061e
saalim <ksaalim418@gmail.com>
2024-07-20 15:18:57 +0530

    vpc block

---
+++ Vpc_Networking/vpc.tf
@@ -0,0 +1,13 @@
+############## VPC ########################
+
+
+resource "aws_vpc" "tcw-vpc" {
+  cidr_block       = var.cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support = var.enable_dns_support
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
