3d49f4d6a12499ef67fb5f098b36259e1d724341
saalim <ksaalim418@gmail.com>
2024-07-21 16:07:49 +0530

    updated code variable

--- Vpc_Networking/variable.tf
+++ Vpc_Networking/variable.tf
@@ -1,14 +1,14 @@
 variable "availability_zone" {
-        type = string
-        description = "availability zone"
-        default = "ap-south-1a"
-  
+  type        = string
+  description = "availability zone"
+  default     = "ap-south-1a"
+
 }
 variable "vpc_name" {
-        type = string
-        description = "vpc name"
-        default = "my-vpc-main"
-  
+  type        = string
+  description = "vpc name"
+  default     = "my-vpc-main"
+
 }
 variable "enable_dns_hostnames" {
   description = "Enable DNS Hostname"
@@ -17,24 +17,24 @@ variable "enable_dns_hostnames" {
 }
 
 variable "enable_dns_support" {
-        description = "Enable_dns_support"
-        type = string
-        default = null
-  
+  description = "Enable_dns_support"
+  type        = string
+  default     = null
+
 }
 variable "cidr" {
-        description = "Enter the CIDR range required for VPC"
-        type = string
-        default = "192.168.0.0/16"
-  
+  description = "Enter the CIDR range required for VPC"
+  type        = string
+  default     = "192.168.0.0/16"
+
 }
 
 
 variable "IGW_tag" {
-        description = "Mention Tag needs to be associated with internet gateway"
-        type = string
-        default = "tcw_igw"
-  
+  description = "Mention Tag needs to be associated with internet gateway"
+  type        = string
+  default     = "tcw_igw"
+
 }
 
 variable "public_subnets_cidr_1" {
@@ -50,13 +50,13 @@ variable "public_subnets_cidr_2" {
 
 variable "database_subnets_cidr_1" {
   description = "mention the CIDR block for database subnet"
-  type = string
-  default = "192.168.5.0/24"
+  type        = string
+  default     = "192.168.5.0/24"
 }
 variable "database_subnets_cidr_2" {
   description = "mention the CIDR block for database subnet"
-  type = string
-  default = "192.168.6.0/24"
+  type        = string
+  default     = "192.168.6.0/24"
 }
 variable "public_subnet_tag_1" {
   description = "Tag for public subnet"
