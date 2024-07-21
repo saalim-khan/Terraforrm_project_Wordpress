b638f468468453a11016fd7c96760401cf397396
saalim <ksaalim418@gmail.com>
2024-07-21 16:08:28 +0530

    updated code vpc

--- Vpc_Networking/vpc.tf
+++ Vpc_Networking/vpc.tf
@@ -2,10 +2,10 @@
 
 
 resource "aws_vpc" "tcw-vpc" {
-  cidr_block       = var.cidr
-  instance_tenancy = "default"
+  cidr_block           = var.cidr
+  instance_tenancy     = "default"
   enable_dns_hostnames = var.enable_dns_hostnames
-  enable_dns_support = var.enable_dns_support
+  enable_dns_support   = var.enable_dns_support
   tags = {
     Name = var.vpc_name
   }
