98e9d66c38cc86e012ab1b3dcb36f00e7d84dc85
saalim <ksaalim418@gmail.com>
2024-07-21 16:09:10 +0530

    updated code subnet

--- Vpc_Networking/subnets.tf
+++ Vpc_Networking/subnets.tf
@@ -2,16 +2,16 @@
 # public Subnet
 #############################################################
 resource "aws_subnet" "tcw_public_subnet_az_1a" {
-  vpc_id     = aws_vpc.tcw-vpc.id
-  cidr_block = var.public_subnets_cidr_1
+  vpc_id            = aws_vpc.tcw-vpc.id
+  cidr_block        = var.public_subnets_cidr_1
   availability_zone = data.aws_availability_zones.available_1.names[0]
   tags = {
     Name = var.public_subnet_tag_1
   }
 }
 resource "aws_subnet" "tcw_public_subnet_az_1b" {
-  vpc_id     = aws_vpc.tcw-vpc.id
-  cidr_block = var.public_subnets_cidr_2
+  vpc_id            = aws_vpc.tcw-vpc.id
+  cidr_block        = var.public_subnets_cidr_2
   availability_zone = data.aws_availability_zones.available_1.names[1]
   tags = {
     Name = var.public_subnet_tag_2
@@ -21,16 +21,16 @@ resource "aws_subnet" "tcw_public_subnet_az_1b" {
 #database Subnet
 ######################################################################
 resource "aws_subnet" "tcw_database_subnet_az_1a" {
-  vpc_id     = aws_vpc.tcw-vpc.id
-  cidr_block = var.database_subnets_cidr_1
+  vpc_id            = aws_vpc.tcw-vpc.id
+  cidr_block        = var.database_subnets_cidr_1
   availability_zone = data.aws_availability_zones.available_1.names[0]
   tags = {
     Name = var.database_subnet_tag_1
   }
 }
 resource "aws_subnet" "tcw_database_subnet_az_1b" {
-  vpc_id     = aws_vpc.tcw-vpc.id
-  cidr_block = var.database_subnets_cidr_2
+  vpc_id            = aws_vpc.tcw-vpc.id
+  cidr_block        = var.database_subnets_cidr_2
   availability_zone = data.aws_availability_zones.available_1.names[1]
   tags = {
     Name = var.database_subnet_tag_2
